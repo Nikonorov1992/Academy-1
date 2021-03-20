da5113bb801a2856e5c264fe00640d455de05d9a
Starinin Andrey <starininandrey@gmail.com>
2021-03-20 13:19:51 +0300

    Добавление методов ToString()

--- Academy/Person.h
+++ Academy/Person.h
@@ -5,10 +5,30 @@
 using namespace std;
 
 enum Sex { Male, Female, Unknown };
+string SexToString(Sex sex)
+{
+    switch (sex) {
+        case Male:
+            return "мужской";
+        case Female:
+            return "женский";
+        case Unknown:
+            return "неопределён";
+    }
+}
 
 class Person {
 public:
 	string name;
 	int age;
 	Sex sex;
+
+	string ToString()
+    {
+	    string temp;
+	    temp = "Имя: " + name + "\n";
+	    temp += "Возраст: " + to_string(age) + "\n";
+	    temp += "Пол: " + SexToString(sex) + "\n";
+        return temp;
+    }
 };
