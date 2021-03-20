f3f2f00a2c7e0bc26c5e33e5d67245b71ed2fd2d
Starinin Andrey <starininandrey@gmail.com>
2021-03-20 13:22:45 +0300

    Добавление основного функционала

--- Academy/Academy.cpp
+++ Academy/Academy.cpp
@@ -1,12 +1,51 @@
-﻿// Academy.cpp: определяет точку входа для приложения.
-//
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
+#include "Student.h"
+#include "Teacher.h"
+#include "Manager.h"
+
+#include "menu.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+system("chcp 65001");
+
+    vector<Student> students;
+    vector<Teacher> teachers;
+    vector<Manager> managers;
+
+    bool exit = false;
+    char select;
+    do {
+        ShowMenu();
+        cout << "Введите номер: ";
+        cin >> select;
+
+        switch (select) {
+            case '1': // 1. Добавить студента
+                break;
+            case '2': // 2. Добавить преподавателя
+                break;
+            case '3': // 3. Добавить менеджера
+                break;
+            case '4': // 4. Показать всех студентов
+                break;
+            case '5': // 5. Показать всех преподавателей
+                break;
+            case '6': // 6. Показать всех менеджеров
+                break;
+            case '0': // 0. Выход
+                exit = true;
+                break;
+            default:
+                break;
+        }
+    } while (!exit);
+
+    cout << "До свидания...";
+
 	return 0;
 }
